7ec1e39360661afdf75e2dd3c0b68c49ac4029e0
Dominic Vogl <dominic.vogl@sigikid.de>
2016-07-25 15:25:10 +0200

    update basic config for slicks sliders

--- src/js/slider/slick.js
+++ src/js/slider/slick.js
@@ -2,25 +2,38 @@
 
     $(function() {
 
+        var slickOptions = {
+            prevArrow: '<div class="slick-arrow slick-prev"><span class="icon icon-empire"></span></div>',
+            nextArrow: '<div class="slick-arrow slick-next"><span class="icon icon-rebel"></span></div>'
+        };
+
         $('.mod-slider').each(function() {
 
             var _ = $(this);
-            _.Slick({
+            var options = {
                 infinite: true,
                 slidesToShow: 1,
                 slidesToScroll: 1
-            });
+            };
+
+            $.extend(options, slickOptions);
+
+            _.slick(options);
 
         });
 
         $('.mod-carousel').each(function() {
 
             var _ = $(this);
-            _.slick({
+            var options = {
                 infinite: true,
                 slidesToShow: 3,
                 slidesToScroll: 3
-            });
+            };
+
+            $.extend(options, slickOptions);
+            
+            _.slick(options);
 
         })
 
